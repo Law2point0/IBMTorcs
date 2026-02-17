89cfd65dca752d9c016cf499a3d18e15369a4fdb
Tom Rayner <52078213+tomrayner1@users.noreply.github.com>
2026-02-17 11:25:53 +0000

    simple text-based chatbot

--- RaceEngineer/race_engineer.py
+++ RaceEngineer/race_engineer.py
@@ -1,96 +1,35 @@
-import queue
-import numpy as np
-import sounddevice as sd
-from scipy.io.wavfile import write
-from faster_whisper import WhisperModel
-import tempfile
+import ollama
+from ollama import ResponseError
+import httpx
 
 
-WAKE_WORD = "computer"
-MODEL_SIZE = "small"
-
-SAMPLE_RATE = 16000
-CHANNELS = 1
-
-CHUNK_SECONDS = 1.2
-SILENCE_LIMIT = 1.0
-ENERGY_THRESHOLD = 0.6
-
-model = WhisperModel(MODEL_SIZE, compute_type="int8")
-
-audio_queue = queue.Queue()
-
-
-def audio_callback(indata, frames, time_info, status):
-  audio_queue.put(indata.copy())
-
-
-def get_audio(seconds):
-  frames_needed = int(SAMPLE_RATE * seconds)
-  frames = []
-
-  while sum(len(f) for f in frames) < frames_needed:
-    frames.append(audio_queue.get()) 
-
-  return np.concatenate(frames).flatten()
-
-
-def rms_energy(audio):
-  return np.sqrt(np.mean(audio**2))
-
-
-def transcribe(audio):
-  with tempfile.NamedTemporaryFile(suffix=".wav") as f:
-    write(f.name, SAMPLE_RATE, audio)
-    segments, _ = model.transcribe(f.name)
-    return " ".join(seg.text for seg in segments).lower()
-
-
-def record_until_silence():
-  recorded = []
-  silence_time = 0
-
-  while True:
-    chunk = get_audio(0.2)
-    energy = rms_energy(chunk)
-
-    if energy > ENERGY_THRESHOLD:
-      recorded.append(chunk)
-      silence_time = 0
-    else:
-      silence_time += 0.2
-      if silence_time >= SILENCE_LIMIT:
-        break
-
-  if not recorded:
-    return None
-
-  return np.concatenate(recorded)
+MODEL = 'hf.co/ibm-granite/granite-4.0-h-tiny-GGUF:Q4_K_M' # Granite 4.0 Tiny
+AI_PROMPT = ''
 
 
 def main():
-  with sd.InputStream(
-    samplerate=SAMPLE_RATE,
-    channels=CHANNELS,
-    callback=audio_callback,
-    blocksize=1024,
-  ):
+  try:
     while True:
-      chunk = get_audio(CHUNK_SECONDS)
-      text = transcribe(chunk)
+      prompt = input("")
 
-      if text:
-        print("Heard:", text)
+      response = ollama.chat(
+        model=MODEL,
+        messages=[
+          {
+            'role': 'system',
+            'content': AI_PROMPT
+          },
+          {
+            'role': 'user',
+            'content': f'{prompt}'
+          }
+        ]
+      )
+  except ResponseError:
+    print(f'Ollama error, make sure you have pulled granite using: ollama pull {MODEL}')
+  except httpx.ConnectError:
+    print('Ollama error, make sure ollama is running in the background.')
 
-      if WAKE_WORD in text:
-        print("Wake word detected!")
 
-        command_audio = record_until_silence()
-        if command_audio is not None:
-          command = transcribe(command_audio)
-          print("Command:", command)
-
-        print("\nWaiting for wake word...\n")
-
-if __name__ == "__main__":
+if __name__ == '__main__':
   main()
