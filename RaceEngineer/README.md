8e2190fc6e4b5c2b04140e5c59a684ca1a27cacf
Tom Rayner <52078213+tomrayner1@users.noreply.github.com>
2026-02-19 14:13:26 +0000

    make the development folder look nice

--- RaceEngineer/README.md
+++ RaceEngineer/README.md
@@ -1,7 +1,9 @@
 # Race Engineer
 
-## Building 
+A few examples used when making the race engineer. Other examples can be found in previous commits in the `race-engineer-chatbot` branch.
 
-Run this to install the required depenendencies.
+## Overview
 
-`pip install -r requirements.txt`
+**driving_example/** - Example driving code with the race engineer built-in, run `torcs_jm_par.py` in that folder.
+
+**race_engineer_demo.py** - Race engineer using an example snapshot of data from when the TORCS car had crashed.
--- RaceEngineer/race_engineer_demo.py
+++ RaceEngineer/race_engineer_demo.py
@@ -8,7 +8,7 @@ AI_PROMPT = 'You are a race engineer. Keep to 1-3 small, simple sentences. You w
 EXAMPLE_TELEMETRY = "{'angle': -2.74844, 'curLapTime': 21.028, 'damage': 285.0, 'distFromStart': 471.785, 'distRaced': 481.785, 'fuel': 93.6591, 'gear': 1.0, 'lastLapTime': 0.0, 'opponents': [200.0, 200.0, 200.0, 200.0, 200.0, 200.0, 200.0, 200.0, 200.0, 200.0, 200.0, 200.0, 200.0, 200.0, 200.0, 200.0, 200.0, 200.0, 200.0, 200.0, 200.0, 200.0, 200.0, 200.0, 200.0, 200.0, 200.0, 200.0, 200.0, 200.0, 200.0, 200.0, 200.0, 200.0, 200.0, 200.0], 'racePos': 1.0, 'rpm': 942.478, 'speedX': 0.0840397, 'speedY': -0.0265828, 'speedZ': 0.000484125, 'track': [-1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0], 'trackPos': -7.46846, 'wheelSpinVel': [0.0, 0.0, 0.0, 0.0], 'z': 0.338315, 'focus': [-1.0, -1.0, -1.0, -1.0, -1.0]}"
 
 
-def prompt_model(prompt, data):
+def prompt_model(prompt):
   response = ollama.chat(
     model=MODEL,
     messages=[
@@ -18,7 +18,7 @@ def prompt_model(prompt, data):
       },
       {
         'role': 'user',
-        'content': f'{prompt}\n\nSnapshot TORCS data: {data}'
+        'content': f'{prompt}\n\nSnapshot TORCS data: {EXAMPLE_TELEMETRY}'
       }
     ],
     stream=True
